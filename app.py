e3f37f8e26f38267080c416cb8a94e4a7811b94a
mazatruc <93188464+mazatruc@users.noreply.github.com>
2024-11-20 17:53:08 +0100

    test explo

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ FILE_PATH = "form_data.txt"
 def index():
     """Page d'accueil."""
     return render_template('index.html')
+
+
 @app.route('/form', methods=['GET', 'POST'])
-
-
 def form():
     """Page de formulaire pour ajouter des contacts."""
     if request.method == 'POST':
